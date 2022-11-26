c21655020080ba9a8216c68212f8e56a7330c075
miyagishihinata <117428868+miyagishihinata@users.noreply.github.com>
2022-11-26 03:02:10 +0000

    [4] 4-27

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
+  get 'books/new'
+  get 'books/index'
+  get 'books/show'
+  get 'books/destroy'
   root to: "homes#top"
   devise_for :users
 
