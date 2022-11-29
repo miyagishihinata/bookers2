c25da5ec50b5c832115221b208ad12a75664a5f0
miyagishihinata <117428868+miyagishihinata@users.noreply.github.com>
2022-11-29 04:46:23 +0000

    [13] 4-27

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
+  get 'users/edit' => 'users#edit'
+  get 'users/:id' => 'users#show'
+
   get 'books/new'
   get 'books/index'
   get 'books/show'
   get 'books/destroy'
-  get 'home/about' => "homes#about"
-  root to: "homes#top"
+  get 'home/about' => "homes#about", as:'about'
+  root to: "homes#top", as:'homes'
   devise_for :users
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
