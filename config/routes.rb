7510ad3fdef65b9e70d06109c58eeae012dbc2bd
miyagishihinata <117428868+miyagishihinata@users.noreply.github.com>
2022-12-03 06:05:10 +0000

    [24] 4-27

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root to: "homes#top", as:'homes'
   devise_for :users
 
-  resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :books, only: [:new, :create, :index, :show, :edit, :destroy]
   resources :users, only: [:show, :edit, :index]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
